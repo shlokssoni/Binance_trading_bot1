286eaf66b051097e77cfffc1e1960668ee1717c2
shlokssoni <whonosefirst@gmail.com>
2026-01-29 16:10:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import argparse
 import logging
 from client_wrapper import BinanceBot
 
-# 1. Setup Logging [cite: 31]
-# This creates a file called 'trading.log' to record your results.
+
 logging.basicConfig(
     filename='trading.log',
     level=logging.INFO,
@@ -11,8 +10,7 @@ logging.basicConfig(
 )
 
 def main():
-    # 2. The "Argument Parser" 
-    # This defines the "flags" you can type in your terminal.
+   
     parser = argparse.ArgumentParser(description="Binance Trading Bot CLI")
     
     parser.add_argument("--symbol", required=True, help="e.g., BTCUSDT")
@@ -23,12 +21,12 @@ def main():
 
     args = parser.parse_args()
 
-    # 3. Connect to your Bot
+   
     bot = BinanceBot()
 
     print(f"\n--- Sending {args.type} {args.side} Order ---")
     
-    # 4. Call the Bot with the info you typed [cite: 29, 30]
+   
     response = bot.place_order(
         symbol=args.symbol,
         side=args.side,
@@ -37,16 +35,16 @@ def main():
         price=args.price
     )
 
-    # 5. Handle the Result [cite: 25-28]
     if "error" in response:
         print(f"FAILED: {response['error']}")
         logging.error(f"Order Failed: {response['error']}")
     else:
-        # Success output as required by the assignment
+      
         print(f"SUCCESS!")
         print(f"Order ID: {response.get('orderId')}")
         print(f"Status: {response.get('status')}")
         logging.info(f"Order Placed Successfully: {response}")
 
 if __name__ == "__main__":
+
     main()
